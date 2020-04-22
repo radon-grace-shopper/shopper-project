40ef44bcb2d612e266e2b8f2c545b4a78915a8c0
SJMawn <SJmawn@gmail.com>
2020-04-22 12:02:01 -0500

    added justification for categories routes

--- server/api/categories.js
+++ server/api/categories.js
@@ -1,6 +1,11 @@
 const router = require('express').Router()
 module.exports = router
 const {Product, Review} = require('../db/models')
+/*
+separated the categories and products search so that all products
+and one product could be obtained regardless of whether a category
+had been selected. we may not need to obtain data that is separated in this way, so this route may be extraneous
+*/
 
 console.log('hitting the category route')
 
--- server/api/index.js
+++ server/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
-router.use('/categories', require('./categories'))
+router.use('/category', require('./categories'))
 router.use('/products', require('./products'))
 
 router.use((req, res, next) => {
