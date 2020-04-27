ead8cd88fb3906c176dd794337e6933969f7960b
Mark Brooks <brooksmark3@yahoo.com>
2020-04-27 16:27:29 -0500

    finished checkout except for stripe

--- server/stripe/index.js
+++ server/stripe/index.js
@@ -1,15 +1,16 @@
 const stripe = require('stripe')('sk_test_XYhLy8AptzhSQpJKehpQ6tOT00BqOnfQTq')
 const router = require('express').Router()
-module.exports = router
 
-router.use('/sercret', async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     const intent = await stripe.paymentIntents.create({
       amount: 1099,
       currency: 'usd'
     })
-    res.json({clientSecret: intent.clientSecret})
+    res.json(intent)
   } catch (err) {
     next(err)
   }
 })
+
+module.exports = router
